1a628d34efa7705e6793b9e79ab74286e729a1c0
unknown <jyotirmayamajhi1998@gmail.com>
2021-10-31 14:08:44 +0530

    Checking the both lines are equal or not

---
+++ src/com/bridgelabz/demo/linecomparision_uc2.java
@@ -0,0 +1,55 @@
+package com.bridgelabz.demo;
+import java.util.Scanner;
+public class linecomparision_uc2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int x1=0,x2=0,y1=0,y2=0;
+		System.out.println("Enter x1,x2,y1,y2 values");
+		
+		System.out.println("x1");
+		x1= sc.nextInt();
+		
+		System.out.println("x2");
+		x2= sc.nextInt();
+		
+		System.out.println("y1");
+		y1= sc.nextInt();
+		
+		System.out.println("y2");
+		y2= sc.nextInt();
+		
+		int length1=(int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		Integer len1=length1;
+		System.out.println("Length of line=" +length1);
+		
+		int p1,p2,q1,q2;
+		System.out.println("Enter p1,p2,q1,q2 values");
+		
+		System.out.println("p1");
+		p1= sc.nextInt();
+		
+		System.out.println("p2");
+		p2= sc.nextInt();
+		
+		System.out.println("q1");
+		q1= sc.nextInt();
+		
+		System.out.println("q2");
+		q2= sc.nextInt();
+		int length2=(int)Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
+		Integer len2=length2;
+		System.out.println("Length of line=" +length2);
+		
+		if(len1 ==len2) {
+			System.out.println("Two lines are equal");
+		}
+		else
+		{
+			System.out.println("Two lines are not equal");
+		}
+		
+	}
+
+}
